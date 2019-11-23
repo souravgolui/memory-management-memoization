d6aeed678f18d638ec155942a880bc926a7a75fc
souravgolui <souravgolui1992@gmail.com>
2019-11-24 00:07:53 +0530

    add example of loop

--- loopExe.js
+++ loopExe.js
@@ -1,3 +1,34 @@
+/**
+ * @description simple way
+ * @param {number} n is a input
+ */
+function prinValue(n) {
+    let result = 0;
+    for (let i= 0; i<=n; i++) {
+        for (let j=0; j<=n; j++) {
+             result += 1;
+        }
+    }
+    return result;
+}
+
+console.log(printVal(500));
+console.log(printVal(500));
+console.log(printVal(500));
+console.log(printVal(500));
+console.log(printVal(500));
+console.log(printVal(500));
+console.log(printVal(500));
+
+
+///////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * @description advanced and optimized way
+ * @param {number} n is input 
+ * @var {array} prevValue store previous calculated value 
+ */
 const prevValue = [];
 function printVal(n) {
     if( prevValue[n] != null) {
